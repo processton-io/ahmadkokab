7ccadc9bd019cbb2b7e14bcef56c13062db5cb36
ahmadkokab <ahmadkokab@processton.com>
2023-09-11 12:07:18 +0500

    CSP Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,9 +112,9 @@ module.exports = {
         mergeStyleHashes: true, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
-          "script-src": "'self' www.google-analytics.com",
-          "style-src": "'self' 'unsafe-inline'",
-          "img-src": "'self' data: www.google-analytics.com"
+          "script-src": "'self' www.google.com",
+          // "style-src": "'self' 'unsafe-inline'",
+          "img-src": "'self' data: www.google.com"
           // you can add your directives or override defaults
         }
       }
