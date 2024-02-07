b11633b7cb361dfdf563d45b703a3294a873c459
Processton <68786248+processton@users.noreply.github.com>
2024-02-07 12:13:10 +0000

    Skills page completed

--- src/templates/skills-list-template.js
+++ src/templates/skills-list-template.js
@@ -13,24 +13,22 @@ export default class SkillsListTemplate extends React.Component {
   render() {
     const posts = this.props.data.allMarkdownRemark.edges
     const categories = ["front_end", "backend" ,"dev_ops","windows_services"]
-    console.log(posts);
     return (
       <Layout nav={true}>
         <div className="mx-auto container max-w-screen-xl lg:my-16 my-8 px-4">
             <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">My Skills</h2>
             <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400"></p>
         </div>
-        {categories.map((category) => { 
-            console.log(category);
+        {categories.map((category) => {
             return <>
-                <h3 className="mb-4 text-2xl lg:text-xl tracking-tight font-bold text-gray-700 dark:text-white">{getCategoryTitle(category)}</h3>
+                <h3 className="mb-4 text-2xl lg:text-xl tracking-tight font-bold text-gray-700 dark:text-white px-4">{getCategoryTitle(category)}</h3>
                 <div className="mx-auto grid max-w-6xl grid-cols-1 gap-3 md:gap-6 md:grid-cols-2 lg:grid-cols-4 px-4">
                     {posts.map(({ node }) => {
                         if(node.frontmatter.type === category){
                             const title = node.frontmatter.title || node.frontmatter.permalink
                             const image = getImage(node.frontmatter.photo?.image)
                             
-                            return <GatsbyLink to={node.frontmatter.permalink} key={node.frontmatter.permalink} className="flex flex-col border px-4 pb-4 bg-white text-black hover:text-white hover:bg-gradient-to-r active:text-white active:bg-gradient-to-r from-sky-800 from-10% via-sky-500 via-30% to-sky-300 to-90% space-y-4 relative overflow-hidden">
+                            return <GatsbyLink to={node.frontmatter.permalink} key={node.frontmatter.permalink} className="flex flex-col border px-4 pb-4 bg-white text-black hover:text-white hover:bg-gradient-to-r active:text-white active:bg-gradient-to-b from-sky-300 from-10% via-sky-500 via-30% to-sky-300 to-90% space-y-4 relative overflow-hidden">
                                 <div class="absolute right-0 top-0 h-16 w-16">
                                     <div
                                         class="absolute transform rotate-45 text-white bg-gradient-to-r from-green-800 from-10% via-green-500 via-30% to-green-300 to-90% text-center text-white font-semibold py-1 right-[-35px] top-[32px] w-[170px]">
